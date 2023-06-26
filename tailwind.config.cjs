c136aa6ba99ecf9208e7672fbb51473a1ee20684
Ben Goshow <goshow@heysparkbox.com>
2023-06-26 10:12:14 -0400

    feat: adds fontsource support and integration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Rubik", ...defaultTheme.fontFamily.sans],
+        'display': ['Bungee', ...defaultTheme.fontFamily.sans],
+        'emphatic': ['Bungee Shade', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
 }
