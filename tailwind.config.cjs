3cb3a668f0eefe75dd23a86ee31073aa37654747
Ben Goshow <goshow@heysparkbox.com>
2023-07-01 09:33:22 -0400

    feat: add menu styling and UI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,11 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        'theme-light': 'hsl(50deg 84% 90%)',
+        'theme-dark': 'hsl(240deg 100% 13%)',
+        'theme-yellow': 'hsl(55deg 100% 50%)',
+      },
       fontFamily: {
         sans: ["Rubik", ...defaultTheme.fontFamily.sans],
         'display': ['Bungee', ...defaultTheme.fontFamily.sans],
