94cb3a2b284b069eb1a17f6036276eec3653f042
Ben Goshow <goshow@heysparkbox.com>
2024-02-03 12:25:55 -0500

    fix: content issues, adding MDX, upgrading Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx'
 
 export default defineConfig({
   // ...
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  })],
-});
+  integrations: [
+    mdx(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+})
--- src/content/missions/unit-converter.md
+++
@@ -1,7 +0,0 @@
----
-layout: '../../Layout.astro'
-title: 'Unit Converter'
-description: 'A React SPA to compare pricing across products with disparate volumes and units of measurement.'
----
-
-This is my first mission.
